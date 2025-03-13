7fb64c86bed10fa25b9d468c7a687ee58648fa30
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-03-12 21:24:14 -0700

    Building FetchedPokemon Model - WIP

---
+++ PokeDex/FetchedPokemon.swift
@@ -0,0 +1,63 @@
+//
+//  FetchedPokemon.swift
+//  PokeDex
+//
+//  Created by Aakash Ambodkar
+//
+
+import Foundation
+
+struct FetchedPokemon: Decodable {
+    let id: Int16
+    let name: String
+    let types: [String]
+    let hp: Int16
+    let attack: Int16
+    let defense: Int16
+    let SpecialAttack: Int16
+    let SpecialDefense: Int16
+    let speed: Int16
+    let sprite: URL
+    let shiny: URL
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case types
+        case states
+        case sprites
+        
+        enum TypeDictionaryKeys: CodingKey {
+            case type
+            
+            enum TypeKeys: CodingKey {
+                case name
+            }
+        }
+        
+        enum StatDictionaryKeys: CodingKey {
+            case baseStat
+        }
+        
+        enum SpriteKeys: String, CodingKey {
+            case sprite = "frontDefault"
+            case shiny = "frontShiny"
+        }
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int16.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.types = try container.decode([String].self, forKey: .types)
+        self.hp = try container.decode(Int16.self, forKey: .hp)
+        self.attack = try container.decode(Int16.self, forKey: .attack)
+        self.defense = try container.decode(Int16.self, forKey: .defense)
+        self.SpecialAttack = try container.decode(Int16.self, forKey: .SpecialAttack)
+        self.SpecialDefense = try container.decode(Int16.self, forKey: .SpecialDefense)
+        self.speed = try container.decode(Int16.self, forKey: .speed)
+        self.sprite = try container.decode(URL.self, forKey: .sprite)
+        self.shiny = try container.decode(URL.self, forKey: .shiny)
+    }
+}
+
