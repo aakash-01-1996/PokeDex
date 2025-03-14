fd12bbb9b22819563d56a563e4ef81369ccc4dce
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-03-13 18:30:53 -0700

    FetchService and getPokemon func

---
+++ PokeDex/FetchService.swift
@@ -0,0 +1,36 @@
+//
+//  FetchService.swift
+//  PokeDex
+//
+//  Created by Aakash Ambodkar
+//
+
+import Foundation
+
+struct FetchService {
+    enum FetchError: Error {
+        case badresponse
+    }
+    
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    func fetchPokemon(_ id: Int) async throws -> FetchedPokemon {
+        let fetchURL = baseURL.appending(path: String(id))
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode ==
+                200 else {
+                throw FetchError.badresponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let pokemon = try decoder.decode(FetchedPokemon.self, from: data)
+        
+        print("Fetched pokemon: \(pokemon.id): \(pokemon.name.capitalized)")
+        return pokemon
+    }
+}
+
