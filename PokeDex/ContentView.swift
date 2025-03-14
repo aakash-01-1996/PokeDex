ed4f5267716e0ec7b3a135e0eef2ae07374f8bcb
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-03-13 18:48:08 -0700

    Design Pokedex list

--- PokeDex/ContentView.swift
+++ PokeDex/ContentView.swift
@@ -19,15 +19,43 @@ struct ContentView: View {
     let fetcher = FetchService()
 
     var body: some View {
-        NavigationView {
+        NavigationStack {
             List {
                 ForEach(pokedex) { pokemon in
-                    NavigationLink {
-                        Text(pokemon.name ?? "no name")
-                    } label: {
-                        Text(pokemon.name ?? "no name")
+                    NavigationLink(value: pokemon) {
+                        AsyncImage(url: pokemon.sprite) {image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 100, height: 100)
+                        
+                        VStack(alignment: .leading) {
+                            Text(pokemon.name!.capitalized)
+                                .fontWeight(.semibold)
+                            
+                            HStack {
+                                ForEach(pokemon.types!, id: \.self) {type in
+                                        Text(type.capitalized)
+                                        .font(.subheadline)
+                                        .fontWeight(.medium)
+                                        .foregroundStyle(.black)
+                                        .padding(.horizontal, 13)
+                                        .padding(.vertical, 5)
+                                        .background(Color(type.capitalized))
+                                        .clipShape(.capsule)
+                                    
+                                }
+                            }
+                        }
                     }
                 }
+            }
+            .navigationTitle("Pokedex")
+            .navigationDestination(for: Pokemon.self) { pokemon in
+                Text(pokemon.name ?? "no name")
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -41,6 +69,7 @@ struct ContentView: View {
             }
         }
     }
+    
     private func getPokemon() {
         Task {
             for id in 1..<152 {
