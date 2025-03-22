3c683b416d170a50423e67d95654790036e9be97
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-03-21 20:09:34 -0700

    Widget: Different View Types

--- PokeDexWidget/PokeDexWidget.swift
+++ PokeDexWidget/PokeDexWidget.swift
@@ -56,11 +56,73 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct PokeDexWidgetEntryView : View {
+    @Environment(\.widgetFamily) var widgetSize
     var entry: Provider.Entry
+    
+    var typesView: some View {
+        ForEach(entry.types, id: \.self) {
+            type in
+            Text(type.capitalized)
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundStyle(.black)
+                .padding(.horizontal, 13)
+                .padding(.vertical, 5)
+                .background(Color(type.capitalized))
+                .clipShape(.capsule)
+                .shadow(radius: 3)
+        }
+    }
+    
+    var pokemonImage: some View {
+        entry.sprite
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .shadow(color: .black, radius: 6)
+    }
 
     var body: some View {
-        VStack {
-            entry.sprite
+        switch widgetSize {
+        case .systemMedium:
+            HStack {
+                pokemonImage
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.largeTitle)
+                        .padding(.vertical, 1)
+                    
+                    HStack {
+                        typesView
+                    }
+                }
+                .layoutPriority(1)
+                
+                Spacer()
+            }
+        case .systemLarge:
+            ZStack {
+                pokemonImage
+                
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.largeTitle)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.75)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        typesView
+                    }
+                }
+            }
+        default:
+            pokemonImage
         }
     }
 }
@@ -72,19 +134,34 @@ struct PokeDexWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 PokeDexWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                    .foregroundStyle(.black)
+                    .containerBackground(Color(entry.types[0].capitalized), for: .widget)
             } else {
                 PokeDexWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Pokemon")
+        .description("Catch em all!")
     }
 }
 
 #Preview(as: .systemSmall) {
+    PokeDexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemMedium) {
+    PokeDexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemLarge) {
     PokeDexWidget()
 } timeline: {
     SimpleEntry.placeholder
