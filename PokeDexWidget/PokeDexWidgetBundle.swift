29a67302cd3d769dcd1318347283e2bbc6af2674
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-03-21 18:51:43 -0700

    Adding Widget to HomeScreen - Creating a New Branch

---
+++ PokeDexWidget/PokeDexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PokeDexWidgetBundle.swift
+//  PokeDexWidget
+//
+//  Created by Aakash Ambodkar on 3/21/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PokeDexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PokeDexWidget()
+    }
+}
